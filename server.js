28f27d2dff61e17cf3ee09fc1a2d6cd48ece5210
JoeZ <josephzasiebida@gmail.com>
2021-02-12 10:09:36 -0600

    'broke'

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ connectDB();
 // Init Middleware
 app.use(express.json());
 
+console.log(process.env.NODE_ENV)
+
+
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
@@ -17,6 +20,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
+    console.log('working')
     // Set static folder
     app.use(express.static('client/build'));
 
@@ -24,6 +28,8 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+
+console.log(process.env.NODE_ENV)
 
 const PORT = process.env.PORT || 5000;
 
